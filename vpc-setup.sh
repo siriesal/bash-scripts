381890e6d57310b25a0a54934bc98b699960084a
Muhammad Fahrizal Rahman <riesal@gmail.com>
2016-05-10 10:43:30 +0700

    add isDone function to replace echo -e

--- vpc-setup.sh
+++ vpc-setup.sh
@@ -7,6 +7,7 @@ clear
 DATACENTER=$1
 AVAILZONE=( a b )
 
+isDone() { echo -e "..DONE"; }
 
 function createVPC() {
 
@@ -29,31 +30,31 @@ if [[ $aEIP -ge 5 ]]; then
   echo -e "You have reach out maximum EIP quota, please set EIP quota to larger than 5.\nScript aborted.\n"
   exit 0
 else
-  echo -e "\nSetup VPC in $DATACENTER zone.."
-  echo -e ".. this may take a while.\n"
+  echo -e "\nSetup VPC in $DATACENTER zone..\n.. this may take a while.\n"
 
   echo -e "Setup 192.168.0.0/16 cidr block"
   VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/16 --region $DATACENTER --output text | awk '{print $6}')
-  echo -e "VPC network 10.0.0.0/16setup.. DONE"
+  isDone
+  sleep 5
 
   VPC_STATE=$(aws ec2 describe-vpcs --region $DATACENTER --output text | grep $VPC_ID | awk '{print $6}')
   echo -e "VPC Status: $VPC_STATE\n"
-  sleep 5
+  sleep 5  
 
   echo -e "Add Octopus $DATACENTER in VPC Tag"
   aws ec2 create-tags --resource $VPC_ID --tags Key=Name,Value="Octopus $DATACENTER" --region $DATACENTER
   sleep 1
-  echo -e "VPC has been tagged successfully\n"
+  isDone
 
   echo -e "Setup internet gateway.."
   IGW_ID=$(aws ec2 create-internet-gateway --region $DATACENTER --output text | awk {'print $2'})
   sleep 5
-  echo -e "Internet gateway setup.. DONE\n"
+  isDone
 
   echo -e "Attach the $IGW_ID internet-gateway to $VPC_ID"
   ATT_IGW_ID=$(aws ec2 attach-internet-gateway --internet-gateway-id $IGW_ID --vpc-id $VPC_ID --region $DATACENTER)
   sleep 1
-  echo -e "Internet gateway attached to VPC.. DONE\n"
+  isDone
 
   echo -e "Setup 2 Public Subnets"
 
@@ -78,40 +79,40 @@ else
 
   echo -e "\nAllocate New Elastic IP.."
   myEIP=$(aws ec2 allocate-address --domain vpc --region $DATACENTER --output text | awk '{print $1}' | tr -d '[[:space:]]')
-  echo -e "..DONE"
+  isDone
 
   echo -e "\nGet ID of Public Subnet A.."
   PUBLIC_SUBNET_A=$(aws ec2 describe-subnets --region $DATACENTER --output text | grep $VPC_ID | grep 192.168.0.0/24 | awk '{print $8}' | tr -d '[[:space:]]')
-  echo -e "..DONE"
+  isDone
 
   echo -e "\nSetup NAT Gateway to Public Subnet A"
   myNATGW=$(aws ec2 create-nat-gateway --subnet-id $PUBLIC_SUBNET_A --allocation-id $myEIP --region $DATACENTER --output text)
-  echo -e "..DONE"
+  isDone
 
   echo -e "\nGet all of public-subnet-id"
   pubSubnetA=$(aws ec2 describe-subnets --region $DATACENTER --output text | grep $VPC_ID | grep 192.168.0.0/24 | awk '{print $8}' | tr -d '[[:space:]]')
   pubSubnetB=$(aws ec2 describe-subnets --region $DATACENTER --output text | grep $VPC_ID | grep 192.168.1.0/24 | awk '{print $8}' | tr -d '[[:space:]]')
-  echo -e "..DONE"
+  isDone
 
   echo -e "\nGet the default route-table-id "
   rTableIdDef=$(aws ec2 describe-route-tables --region $DATACENTER --output text | grep $VPC_ID | awk '{print $2}' | tr -d '[[:space:]]')
-  echo -e "..DONE"
+  isDone
 
   echo -e "\nAssociate all public subnet with default table"
   aws ec2 associate-route-table --route-table-id $rTableIdDef --subnet-id $pubSubnetA --region $DATACENTER
   aws ec2 associate-route-table --route-table-id $rTableIdDef --subnet-id $pubSubnetB --region $DATACENTER
-  echo -e "..DONE"
+  isDone
 
   sleep 10
 
   echo -e "\nAdd routing rule for internet-gateway to default route"
   aws ec2 create-route --route-table-id $rTableIdDef --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID --region $DATACENTER
   sleep 3
-  echo -e "..DONE"
+  isDone
 
   echo -e "\nSetup new route table for internal-access"
   rTableIdPri=$(aws ec2 create-route-table --vpc-id $VPC_ID --region $DATACENTER --output text | grep ROUTETABLE | grep $VPC_ID | awk '{print $2}' | tr -d '[[:space:]]')
-  echo -e "..DONE"
+  isDone
 
   echo -e "\nNAT Gateway ID: $rTableIdPri \n"
   sleep 10
@@ -119,7 +120,7 @@ else
   echo -e "\nGet both of Private Subnet zone.."
   priSubnetA=$(aws ec2 describe-subnets --region $DATACENTER --output text | grep $VPC_ID | grep 192.168.2.0/24 | awk '{print $8}' | tr -d '[[:space:]]')
   priSubnetB=$(aws ec2 describe-subnets --region $DATACENTER --output text | grep $VPC_ID | grep 192.168.3.0/24 | awk '{print $8}' | tr -d '[[:space:]]')
-  echo -e "..DONE"
+  isDone
 
   sleep 5
 
@@ -127,7 +128,7 @@ else
   myNATGW=$(aws ec2 describe-nat-gateways --region $DATACENTER --output text | grep -v "NATGATEWAYADDRESS" | grep $VPC_ID | awk '{print $3}' | tr -d '[[:space:]]')
   sleep 3
   aws ec2 create-route --route-table-id $rTableIdPri --destination-cidr-block 0.0.0.0/0 --gateway-id $myNATGW --region $DATACENTER
-  echo -e "..DONE\n"
+  isDone
 
   echo -e "\nAssociate all public subnet with default table"
   aws ec2 associate-route-table --route-table-id $rTableIdPri --subnet-id $priSubnetA --region $DATACENTER
