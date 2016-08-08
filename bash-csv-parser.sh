a2e8e8623b8b2ffae233ee56e9b59baf5149d7bc
riesal <riesal@gmail.com>
2016-08-08 16:18:29 +0700

    create bash-csv-parser.sh
    csv parser using bash and IFS.

---
+++ bash-csv-parser.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# riesal@gmail.com
+# usage: $ bash bash-csv-parser.sh servers.csv result.txt
+
+input=$1
+output=$2
+
+# use ";" as the field separator using $IFS 
+# and read line by line using while read combo 
+
+while IFS=';' read -r f1 f2 f3 f4 f5 f6 f7
+do
+  echo "$f1 $f2 $f3 $f4 $f5 $f6 $f7" >> text.txt
+done < "$input"
+
+awk '{print $2}' text.txt | cut -d'"' -f2 > $output
